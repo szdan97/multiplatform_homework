0ad0f8545f8b829c74f13d024d40ce51294dce58
Daniel <szekeresdani@gmail.com>
2020-04-04 00:23:45 +0200

    build added

---
+++ build/precache-manifest.51e5e84f88df35e97b50eec57882d139.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d25474f39aa7e26ca486665ea3a8a2e3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e28291b55760bf99005b",
+    "url": "./static/css/main.fc33600b.chunk.css"
+  },
+  {
+    "revision": "894daed5cac13c915747",
+    "url": "./static/js/2.ee16ff66.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.ee16ff66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e28291b55760bf99005b",
+    "url": "./static/js/main.22786c17.chunk.js"
+  },
+  {
+    "revision": "821e3e9e8623f25acdce",
+    "url": "./static/js/runtime-main.62724d6a.js"
+  }
+]);
